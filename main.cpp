769e5737f5bf4210a13d8f67684179dcf1498e42
UMANG AGARWAL <39197141+UMANG380@users.noreply.github.com>
2018-05-20 03:56:45 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+
+
+int fibonacci_last_digit(long long n) {
+    if (n <= 1)
+        return n;
+
+    int previous = 0;
+    int current = 1;
+
+    for (int i = 0; i < n - 1; ++i) {
+        int tmp_previous = previous % 10;
+        previous = current % 10;
+        current = tmp_previous + current % 10;
+    }
+    return current % 10;
+}
+
+
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int c = fibonacci_last_digit(n);
+    std::cout << c << '\n';
+}
